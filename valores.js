26b339322f52a3647d3cb574163b3315d08b3b22
xiagoz <christ2194.gn@gmail.com>
2021-08-02 23:06:06 -0500

    Tipos de valores y varibles

---
+++ valores.js
@@ -0,0 +1,18 @@
+//number
+40
+//string
+"Christian gómez"
+//booleanos
+true
+false
+//Valores placeholder
+null
+undefined
+//arrays
+[1,2,3]
+//valor tipo obejto
+{ nombre: "Christian" }
+
+//Como saber el tipo de datos de un valor
+//Podemos utilizar la función typeOf([valor])
+typeof(40) //Number
---
+++ variables.js
@@ -0,0 +1,11 @@
+var nombre = "Christian";
+// Solo declarar una variable
+var edad;
+// Inicializar la variable
+edad = 27;
+
+var elementos = ["Computadora", "Celular"];
+var persona = {
+    nombre: "Chris",
+    edad: 27
+}
