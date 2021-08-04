2fa3b739bf601fe41f2c0f7ef1fe99bfa8ff6758
xiagoz <christ2194.gn@gmail.com>
2021-08-04 00:43:56 -0500

    funciones y scope

---
+++ funciones.js
@@ -0,0 +1,14 @@
+// Existen dos tipos de funciones
+// Declarativas
+
+function myFunction() {
+    return 3;
+}
+
+// Expresión o tambien se conocen como funciones anonimas
+// Parametros dentro de la función
+var myFunction = function(a,b){
+    return a + b;
+}
+
+myFunction();
